f9ea93969ef526bd1df38e689c634c838b46c478
Luiz Felipe <luizfelipe0527@gmail.com>
2024-01-16 20:05:09 -0300

    feat: implement CustomResponseBody class

--- src/main/java/com/felipe/projectmanagerapi/controllers/UserController.java
+++ src/main/java/com/felipe/projectmanagerapi/controllers/UserController.java
@@ -1,14 +1,20 @@
 package com.felipe.projectmanagerapi.controllers;
 
+import com.felipe.projectmanagerapi.dtos.LoginDTO;
 import com.felipe.projectmanagerapi.dtos.UserRegisterDTO;
 import com.felipe.projectmanagerapi.dtos.UserResponseDTO;
+import com.felipe.projectmanagerapi.enums.ResponseConditionStatus;
 import com.felipe.projectmanagerapi.services.UserService;
-import org.springframework.http.ResponseEntity;
+import com.felipe.projectmanagerapi.utils.CustomResponseBody;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -26,8 +32,30 @@ public class UserController {
   }
 
   @PostMapping("/auth/register")
-  public ResponseEntity<UserResponseDTO> register(@RequestBody UserRegisterDTO body) {
-    UserResponseDTO response = this.userService.register(body);
-    return ResponseEntity.ok().body(response);
+  @ResponseStatus(HttpStatus.CREATED)
+  public CustomResponseBody<UserResponseDTO> register(@RequestBody UserRegisterDTO data) {
+    UserResponseDTO registerResponseDTO = this.userService.register(data);
+
+    CustomResponseBody<UserResponseDTO> response = new CustomResponseBody<>();
+    response.setStatus(ResponseConditionStatus.SUCCESS);
+    response.setCode(HttpStatus.CREATED);
+    response.setMessage("Usuário criado com sucesso");
+    response.setData(registerResponseDTO);
+
+    return response;
+  }
+
+  @PostMapping("/auth/login")
+  @ResponseStatus(HttpStatus.OK)
+  public CustomResponseBody<Map<String, Object>> login(@RequestBody LoginDTO login) {
+    Map<String, Object> loginResponseMap = this.userService.login(login);
+
+    CustomResponseBody<Map<String, Object>> response = new CustomResponseBody<>();
+    response.setStatus(ResponseConditionStatus.SUCCESS);
+    response.setCode(HttpStatus.OK);
+    response.setMessage("Usuário logado");
+    response.setData(loginResponseMap);
+
+    return response;
   }
 }
