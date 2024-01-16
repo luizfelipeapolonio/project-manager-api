989eff95d383585154f524642b13b080bdf3a0ea
Luiz Felipe <luizfelipe0527@gmail.com>
2024-01-15 23:42:49 -0300

    feat: add SecurityFilter to filter chain

---
+++ src/main/java/com/felipe/projectmanagerapi/infra/security/SecurityConfiguration.java
@@ -0,0 +1,52 @@
+package com.felipe.projectmanagerapi.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+  private final SecurityFilter securityFilter;
+
+  public SecurityConfiguration(SecurityFilter securityFilter) {
+    this.securityFilter = securityFilter;
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+      .csrf(AbstractHttpConfigurer::disable)
+      .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+      .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+        .requestMatchers(HttpMethod.POST, "/api/auth/register").permitAll()
+        .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+        .requestMatchers(HttpMethod.GET, "/api/auth/test").hasRole("READ_ONLY")
+        .anyRequest().authenticated())
+      .addFilterBefore(this.securityFilter, UsernamePasswordAuthenticationFilter.class)
+      .build();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+    AuthenticationConfiguration authenticationConfiguration
+  ) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder(12);
+  }
+}
