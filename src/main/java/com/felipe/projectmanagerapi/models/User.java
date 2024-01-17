d7e7833c41a5bcfe1f29645bfa8a537cf02745c4
Luiz Felipe <luizfelipe0527@gmail.com>
2024-01-17 14:03:17 -0300

    change: adjust getters and setters methods

--- src/main/java/com/felipe/projectmanagerapi/models/User.java
+++ src/main/java/com/felipe/projectmanagerapi/models/User.java
@@ -99,7 +99,15 @@ public class User {
     return this.createdAt;
   }
 
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+
   public LocalDateTime getUpdatedAt() {
     return this.updatedAt;
+  }
+
+  public void setUpdatedAt(LocalDateTime updatedAt) {
+    this.updatedAt = updatedAt;
   }
 }
