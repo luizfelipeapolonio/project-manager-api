aa2e530182767bc6f28009cc721995256a46bed4
Luiz Felipe <luizfelipe0527@gmail.com>
2024-01-18 21:54:10 -0300

    improvement: add HandlerExceptionResolver to filter

--- src/main/java/com/felipe/projectmanagerapi/infra/security/SecurityFilter.java
+++ src/main/java/com/felipe/projectmanagerapi/infra/security/SecurityFilter.java
@@ -4,12 +4,14 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
@@ -18,10 +20,16 @@ public class SecurityFilter extends OncePerRequestFilter {
 
   private final TokenService tokenService;
   private final AuthorizationService authorizationService;
+  private final HandlerExceptionResolver resolver;
 
-  public SecurityFilter(TokenService tokenService, AuthorizationService authorizationService) {
+  public SecurityFilter(
+    TokenService tokenService,
+    AuthorizationService authorizationService,
+    @Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver
+  ) {
     this.tokenService = tokenService;
     this.authorizationService = authorizationService;
+    this.resolver = resolver;
   }
 
   @Override
@@ -39,8 +47,7 @@ public class SecurityFilter extends OncePerRequestFilter {
       filterChain.doFilter(request, response);
 
     } catch(Exception exception) {
-      // TODO: trocar para o resolver
-      System.out.println(exception.getMessage());
+      this.resolver.resolveException(request, response, null, exception);
     }
 
   }
