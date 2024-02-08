a1a05ed2b4cea6c8838998d6cc17b89342c4187e
Luiz Felipe <luizfelipe0527@gmail.com>
2024-02-08 19:45:10 -0300

    feat: implement findAllByOwnerId method

--- src/main/java/com/felipe/projectmanagerapi/repositories/WorkspaceRepository.java
+++ src/main/java/com/felipe/projectmanagerapi/repositories/WorkspaceRepository.java
@@ -2,5 +2,13 @@ package com.felipe.projectmanagerapi.repositories;
 
 import com.felipe.projectmanagerapi.models.Workspace;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface WorkspaceRepository extends JpaRepository<Workspace, String> {}
+import java.util.List;
+
+public interface WorkspaceRepository extends JpaRepository<Workspace, String> {
+
+  @Query("SELECT w FROM Workspace w WHERE w.owner.id = :id")
+  List<Workspace> findAllByOwnerId(@Param("id") String ownerId);
+}
