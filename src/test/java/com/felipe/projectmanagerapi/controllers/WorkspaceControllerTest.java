55745883d233ac38ae6b8c762925cf7fdd3cd79e
Luiz Felipe <luizfelipe0527@gmail.com>
2024-02-13 19:58:46 -0300

    test: implement add member to workspace test cases

--- src/test/java/com/felipe/projectmanagerapi/controllers/WorkspaceControllerTest.java
+++ src/test/java/com/felipe/projectmanagerapi/controllers/WorkspaceControllerTest.java
@@ -1,11 +1,16 @@
 package com.felipe.projectmanagerapi.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.felipe.projectmanagerapi.dtos.UserResponseDTO;
 import com.felipe.projectmanagerapi.dtos.WorkspaceCreateOrUpdateDTO;
+import com.felipe.projectmanagerapi.dtos.WorkspaceMembersResponseDTO;
 import com.felipe.projectmanagerapi.dtos.WorkspaceResponseDTO;
+import com.felipe.projectmanagerapi.dtos.mappers.UserMapper;
 import com.felipe.projectmanagerapi.dtos.mappers.WorkspaceMapper;
 import com.felipe.projectmanagerapi.enums.ResponseConditionStatus;
+import com.felipe.projectmanagerapi.exceptions.MemberAlreadyExistsException;
 import com.felipe.projectmanagerapi.exceptions.RecordNotFoundException;
+import com.felipe.projectmanagerapi.models.User;
 import com.felipe.projectmanagerapi.models.Workspace;
 import com.felipe.projectmanagerapi.services.WorkspaceService;
 import com.felipe.projectmanagerapi.utils.CustomResponseBody;
@@ -56,6 +61,9 @@ public class WorkspaceControllerTest {
 
   @SpyBean
   WorkspaceMapper workspaceMapper;
+
+  @SpyBean
+  UserMapper userMapper;
 
   private AutoCloseable closeable;
   private String baseUrl;
@@ -217,5 +225,106 @@ public class WorkspaceControllerTest {
 
     verify(this.workspaceService, times(1)).getAllUserWorkspaces();
     verify(this.workspaceMapper, times(3)).toDTO(any(Workspace.class));
+  }
+
+  @Test
+  @DisplayName("insertMember - Should return a success response with OK status code and the workspace with the inserted member")
+  void insertMemberSuccess() throws Exception {
+    Workspace workspace = this.dataMock.getWorkspaces().get(0);
+
+    WorkspaceResponseDTO workspaceDTO = new WorkspaceResponseDTO(
+      workspace.getId(),
+      workspace.getName(),
+      workspace.getOwner().getId(),
+      workspace.getCreatedAt(),
+      workspace.getUpdatedAt()
+    );
+    List<UserResponseDTO> usersDTO = workspace.getMembers()
+      .stream()
+      .map(member -> new UserResponseDTO(
+        member.getId(),
+        member.getName(),
+        member.getEmail(),
+        member.getRole().getName(),
+        member.getCreatedAt(),
+        member.getUpdatedAt()
+      ))
+      .toList();
+
+    CustomResponseBody<WorkspaceMembersResponseDTO> response = new CustomResponseBody<>();
+    response.setStatus(ResponseConditionStatus.SUCCESS);
+    response.setCode(HttpStatus.OK);
+    response.setMessage("Membro inserido no workspace com sucesso");
+    response.setData(new WorkspaceMembersResponseDTO(workspaceDTO, usersDTO));
+
+    String jsonBody = this.objectMapper.writeValueAsString(response);
+
+    when(this.workspaceService.insertMember("01", "02")).thenReturn(workspace);
+    when(this.workspaceMapper.toDTO(workspace)).thenReturn(workspaceDTO);
+
+    this.mockMvc.perform(patch(this.baseUrl + "/01/member/02")
+      .accept(MediaType.APPLICATION_JSON))
+      .andExpect(content().json(jsonBody));
+
+    verify(this.workspaceService, times(1)).insertMember("01", "02");
+    verify(this.workspaceMapper, times(1)).toDTO(workspace);
+    verify(this.userMapper, times(3)).toDTO(any(User.class));
+  }
+
+  @Test
+  @DisplayName("insertMember - Should return an error response with forbidden status code")
+  void insertMemberFailsByDifferentWorkspaceOwnerId() throws Exception{
+    when(this.workspaceService.insertMember("01", "02"))
+      .thenThrow(new AccessDeniedException("Acesso negado: Você não tem permissão para alterar este recurso"));
+
+    this.mockMvc.perform(patch(this.baseUrl + "/01/member/02")
+      .accept(MediaType.APPLICATION_JSON))
+      .andExpect(status().isForbidden())
+      .andExpect(jsonPath("$.status").value(ResponseConditionStatus.ERROR.getValue()))
+      .andExpect(jsonPath("$.code").value(HttpStatus.FORBIDDEN.value()))
+      .andExpect(jsonPath("$.message").value("Acesso negado: Você não tem permissão para alterar este recurso"))
+      .andExpect(jsonPath("$.data").doesNotExist());
+
+    verify(this.workspaceService, times(1)).insertMember("01", "02");
+    verify(this.workspaceMapper, never()).toDTO(any(Workspace.class));
+    verify(this.userMapper, never()).toDTO(any(User.class));
+  }
+
+  @Test
+  @DisplayName("insertMember - Should return an error response with not found status code")
+  void insertMemberFailsByWorkspaceNotFound() throws Exception {
+    when(this.workspaceService.insertMember("01", "02"))
+      .thenThrow(new RecordNotFoundException("Workspace com ID: '01' não encontrado"));
+
+    this.mockMvc.perform(patch(this.baseUrl + "/01/member/02")
+      .accept(MediaType.APPLICATION_JSON))
+      .andExpect(status().isNotFound())
+      .andExpect(jsonPath("$.status").value(ResponseConditionStatus.ERROR.getValue()))
+      .andExpect(jsonPath("$.code").value(HttpStatus.NOT_FOUND.value()))
+      .andExpect(jsonPath("$.message").value("Workspace com ID: '01' não encontrado"))
+      .andExpect(jsonPath("$.data").doesNotExist());
+
+    verify(this.workspaceService, times(1)).insertMember("01", "02");
+    verify(this.workspaceMapper, never()).toDTO(any(Workspace.class));
+    verify(this.userMapper, never()).toDTO(any(User.class));
+  }
+
+  @Test
+  @DisplayName("insertMember - Should return an error response with conflict status code")
+  void insertMemberFailsByMemberAlreadyExists() throws Exception {
+    when(this.workspaceService.insertMember("01", "02"))
+      .thenThrow(new MemberAlreadyExistsException("02", "01"));
+
+    this.mockMvc.perform(patch(this.baseUrl + "/01/member/02")
+      .accept(MediaType.APPLICATION_JSON))
+      .andExpect(status().isConflict())
+      .andExpect(jsonPath("$.status").value(ResponseConditionStatus.ERROR.getValue()))
+      .andExpect(jsonPath("$.code").value(HttpStatus.CONFLICT.value()))
+      .andExpect(jsonPath("$.message").value("O usuário de ID: '02' já é membro do workspace de ID: '01'."))
+      .andExpect(jsonPath("$.data").doesNotExist());
+
+    verify(this.workspaceService, times(1)).insertMember("01", "02");
+    verify(this.workspaceMapper, never()).toDTO(any(Workspace.class));
+    verify(this.userMapper, never()).toDTO(any(User.class));
   }
 }
--- src/test/java/com/felipe/projectmanagerapi/services/WorkspaceServiceTest.java
+++ src/test/java/com/felipe/projectmanagerapi/services/WorkspaceServiceTest.java
@@ -1,6 +1,7 @@
 package com.felipe.projectmanagerapi.services;
 
 import com.felipe.projectmanagerapi.dtos.WorkspaceCreateOrUpdateDTO;
+import com.felipe.projectmanagerapi.exceptions.MemberAlreadyExistsException;
 import com.felipe.projectmanagerapi.exceptions.RecordNotFoundException;
 import com.felipe.projectmanagerapi.infra.security.AuthorizationService;
 import com.felipe.projectmanagerapi.infra.security.UserPrincipal;
@@ -27,6 +28,7 @@ import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.anyString;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchException;
 
@@ -153,7 +155,7 @@ public class WorkspaceServiceTest {
 
     assertThat(thrown)
       .isExactlyInstanceOf(RecordNotFoundException.class)
-      .hasMessage("Workspace não encontrado");
+      .hasMessage("Workspace com ID: '01' não encontrado");
 
     verify(this.authorizationService, times(1)).getAuthentication();
     verify(this.authentication, times(1)).getPrincipal();
@@ -182,33 +184,113 @@ public class WorkspaceServiceTest {
     verify(this.workspaceRepository, times(1)).findAllByOwnerId(userPrincipal.getUser().getId());
   }
 
-//  @Test
-//  @DisplayName("insertMember - Should successfully insert a user as a member of the workspace and return it")
-//  void insertWorkspaceMemberSuccess() {
-//    User workspaceOwner = this.dataMock.getUsers().get(0);
-//    User workspaceMember = this.dataMock.getUsers().get(1);
-//    Workspace mockWorkspace = this.dataMock.getWorkspaces().get(0);
-//    UserPrincipal userPrincipal = new UserPrincipal(workspaceOwner);
-//
-//    Workspace workspace = new Workspace();
-//    workspace.setId(mockWorkspace.getId());
-//    workspace.setName(mockWorkspace.getName());
-//    workspace.setOwner(workspaceOwner);
-//    workspace.setMembers(List.of(workspaceMember));
-//    workspace.setCreatedAt(mockWorkspace.getCreatedAt());
-//    workspace.setUpdatedAt(mockWorkspace.getUpdatedAt());
-//
-//    when(this.authorizationService.getAuthentication()).thenReturn(this.authentication);
-//    when(this.authentication.getPrincipal()).thenReturn(userPrincipal);
-//    //when(this.userService.getProfile(workspaceMember.getId())).thenReturn(any(UserResponseDTO.class));
-//    when(this.workspaceRepository.findById(workspace.getId())).thenReturn(Optional.of(workspace));
-//    when(this.workspaceRepository.save(workspace)).thenReturn(workspace);
-//
-//    WorkspaceMemberResponseDTO insertedMember = this.workspaceService.insertMember("01", "02");
-//
-//    assertThat(insertedMember.workspace().id()).isEqualTo(workspace.getId());
-//    assertThat(insertedMember.workspace().name()).isEqualTo(workspace.getName());
-//    assertThat(insertedMember.workspace().ownerId()).isEqualTo(workspace.getOwner().getId());
-//    assertThat(insertedMember.members().get(0).id()).isEqualTo(workspace.getMembers().get(0).getId());
-//  }
+  @Test
+  @DisplayName("insertMember - Should successfully insert a user as a member of the workspace and return it")
+  void insertWorkspaceMemberSuccess() {
+    User workspaceOwner = this.dataMock.getUsers().get(0);
+    User workspaceMember = this.dataMock.getUsers().get(1);
+    Workspace mockWorkspace = this.dataMock.getWorkspaces().get(0);
+    UserPrincipal userPrincipal = new UserPrincipal(workspaceOwner);
+
+    Workspace workspace = new Workspace();
+    workspace.setId(mockWorkspace.getId());
+    workspace.setName(mockWorkspace.getName());
+    workspace.setOwner(workspaceOwner);
+    workspace.setCreatedAt(mockWorkspace.getCreatedAt());
+    workspace.setUpdatedAt(mockWorkspace.getUpdatedAt());
+
+    when(this.authorizationService.getAuthentication()).thenReturn(this.authentication);
+    when(this.authentication.getPrincipal()).thenReturn(userPrincipal);
+    when(this.userService.getProfile(workspaceMember.getId())).thenReturn(workspaceMember);
+    when(this.workspaceRepository.findById("01")).thenReturn(Optional.of(workspace));
+    when(this.workspaceRepository.save(workspace)).thenReturn(workspace);
+
+    Workspace insertedMember = this.workspaceService.insertMember("01", "02");
+
+    assertThat(insertedMember.getId()).isEqualTo(workspace.getId());
+    assertThat(insertedMember.getName()).isEqualTo(workspace.getName());
+    assertThat(insertedMember.getOwner().getId()).isEqualTo(workspace.getOwner().getId());
+    assertThat(insertedMember.getMembers()).contains(workspaceMember).hasSize(1);
+
+    verify(this.authorizationService, times(1)).getAuthentication();
+    verify(this.authentication, times(1)).getPrincipal();
+    verify(this.workspaceRepository, times(1)).findById("01");
+    verify(this.workspaceRepository, times(1)).save(workspace);
+    verify(this.userService, times(1)).getProfile(workspaceMember.getId());
+  }
+
+  @Test
+  @DisplayName("insertMember - Should throw a RecordNotFoundException if workspace is not found")
+  void insertWorkspaceMemberFailsByWorkspaceNotFound() {
+    User user = this.dataMock.getUsers().get(0);
+    UserPrincipal userPrincipal = new UserPrincipal(user);
+
+    when(this.authorizationService.getAuthentication()).thenReturn(this.authentication);
+    when(this.authentication.getPrincipal()).thenReturn(userPrincipal);
+    when(this.userService.getProfile(anyString())).thenReturn(any(User.class));
+    when(this.workspaceRepository.findById("01")).thenReturn(Optional.empty());
+
+    Exception thrown = catchException(() -> this.workspaceService.insertMember("01", "02"));
+
+    assertThat(thrown)
+      .isExactlyInstanceOf(RecordNotFoundException.class)
+      .hasMessage("Workspace com ID: '01' não encontrado");
+
+    verify(this.authorizationService, times(1)).getAuthentication();
+    verify(this.authentication, times(1)).getPrincipal();
+    verify(this.userService, times(1)).getProfile(anyString());
+    verify(this.workspaceRepository, times(1)).findById("01");
+    verify(this.workspaceRepository, never()).save(any(Workspace.class));
+  }
+
+  @Test
+  @DisplayName("insertMember - Should throw an AccessDeniedException if the workspace owner id is different from authenticated user id")
+  void insertMemberFailsByDifferentWorkspaceOwnerId() {
+    UserPrincipal userPrincipal = new UserPrincipal(this.dataMock.getUsers().get(1));
+    Workspace workspace = this.dataMock.getWorkspaces().get(0);
+    User workspaceMember = this.dataMock.getUsers().get(2);
+
+    when(this.authorizationService.getAuthentication()).thenReturn(this.authentication);
+    when(this.authentication.getPrincipal()).thenReturn(userPrincipal);
+    when(this.userService.getProfile(anyString())).thenReturn(workspaceMember);
+    when(this.workspaceRepository.findById("01")).thenReturn(Optional.of(workspace));
+
+    Exception thrown = catchException(() -> this.workspaceService.insertMember("01", "02"));
+
+    assertThat(thrown)
+      .isExactlyInstanceOf(AccessDeniedException.class)
+      .hasMessage("Acesso negado: Você não tem permissão para alterar este recurso");
+
+    verify(this.authorizationService, times(1)).getAuthentication();
+    verify(this.authentication, times(1)).getPrincipal();
+    verify(this.userService, times(1)).getProfile(anyString());
+    verify(this.workspaceRepository, times(1)).findById("01");
+    verify(this.workspaceRepository, never()).save(workspace);
+  }
+
+  @Test
+  @DisplayName("insertMember - Should throw a MemberAlreadyExistsException if the given user is already a member of the workspace")
+  void insertMemberFailsByMemberAlreadyExists() {
+    UserPrincipal userPrincipal = new UserPrincipal(this.dataMock.getUsers().get(0));
+    User workspaceMember = this.dataMock.getUsers().get(1);
+    Workspace workspace = this.dataMock.getWorkspaces().get(0);
+    workspace.setMembers(List.of(workspaceMember));
+
+    when(this.authorizationService.getAuthentication()).thenReturn(this.authentication);
+    when(this.authentication.getPrincipal()).thenReturn(userPrincipal);
+    when(this.userService.getProfile("02")).thenReturn(workspaceMember);
+    when(this.workspaceRepository.findById("01")).thenReturn(Optional.of(workspace));
+
+    Exception thrown = catchException(() -> this.workspaceService.insertMember("01", "02"));
+
+    assertThat(thrown)
+      .isExactlyInstanceOf(MemberAlreadyExistsException.class)
+      .hasMessage("O usuário de ID: '02' já é membro do workspace de ID: '01'.");
+
+    verify(this.authorizationService, times(1)).getAuthentication();
+    verify(this.authentication, times(1)).getPrincipal();
+    verify(this.workspaceRepository, times(1)).findById("01");
+    verify(this.userService, times(1)).getProfile("02");
+    verify(this.workspaceRepository, never()).save(workspace);
+  }
 }
