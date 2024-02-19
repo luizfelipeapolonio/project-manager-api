bdb3ead6392ebe464476fc51eeaa70d8b0eed57d
Luiz Felipe <luizfelipe0527@gmail.com>
2024-02-18 22:58:16 -0300

    feat: add Project entity

---
+++ src/main/java/com/felipe/projectmanagerapi/models/Project.java
@@ -0,0 +1,153 @@
+package com.felipe.projectmanagerapi.models;
+
+import com.felipe.projectmanagerapi.enums.PriorityLevel;
+import com.felipe.projectmanagerapi.enums.converters.PriorityLevelConverter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Convert;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "project")
+public class Project {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private String id;
+
+  @Column(length = 70, nullable = false)
+  private String name;
+
+  @Column(length = 40, nullable = false)
+  private String category;
+
+  @Column(nullable = false)
+  private String description;
+
+  @Column(nullable = false)
+  private BigDecimal budget;
+
+  @Convert(converter = PriorityLevelConverter.class)
+  @Column(nullable = false)
+  private PriorityLevel priority;
+
+  @Column(columnDefinition = "DATE", nullable = false)
+  private LocalDate deadline;
+
+  @CreationTimestamp
+  @Column(name = "created_at", columnDefinition = "TIMESTAMP(2)", nullable = false)
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  @Column(name = "updated_at", columnDefinition = "TIMESTAMP(2)", nullable = false)
+  private LocalDateTime updatedAt;
+
+  @ManyToOne(optional = false)
+  @JoinColumn(name = "owner_id", nullable = false)
+  private User owner;
+
+  @ManyToOne(optional = false)
+  @JoinColumn(name = "workspace_id", nullable = false)
+  private Workspace workspace;
+
+  public Project() {}
+
+  public String getId() {
+    return this.id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getCategory() {
+    return this.category;
+  }
+
+  public void setCategory(String category) {
+    this.category = category;
+  }
+
+  public String getDescription() {
+    return this.description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public BigDecimal getBudget() {
+    return this.budget;
+  }
+
+  public void setBudget(BigDecimal budget) {
+    this.budget = budget;
+  }
+
+  public PriorityLevel getPriority() {
+    return this.priority;
+  }
+
+  public void setPriority(PriorityLevel priority) {
+    this.priority = priority;
+  }
+
+  public LocalDate getDeadLine() {
+    return this.deadline;
+  }
+
+  public void setDeadLine(LocalDate deadline) {
+    this.deadline = deadline;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return this.createdAt;
+  }
+
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public LocalDateTime getUpdatedAt() {
+    return this.updatedAt;
+  }
+
+  public void setUpdatedAt(LocalDateTime updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+  public User getOwner() {
+    return this.owner;
+  }
+
+  public void setOwner(User owner) {
+    this.owner = owner;
+  }
+
+  public Workspace getWorkspace() {
+    return this.workspace;
+  }
+
+  public void setWorkspace(Workspace workspace) {
+    this.workspace = workspace;
+  }
+}
