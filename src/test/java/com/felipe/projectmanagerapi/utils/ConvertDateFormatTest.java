1c9474d5a43df3e7ec263e25a34a1c2c3d74957b
Luiz Felipe <luizfelipe0527@gmail.com>
2024-02-22 16:11:48 -0300

    test: add ConvertDateFormat test cases

---
+++ src/test/java/com/felipe/projectmanagerapi/utils/ConvertDateFormatTest.java
@@ -0,0 +1,34 @@
+package com.felipe.projectmanagerapi.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConvertDateFormatTest {
+
+  @Test
+  @DisplayName("convertStringToDateFormat - Should successfully convert a string date of the 'dd-MM-yyyy' format into a LocalDate instance")
+  void convertStringToDateFormatSuccess() {
+    String stringDate = "01-01-2025";
+    DateTimeFormatter outputFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    LocalDate convertedDate = ConvertDateFormat.convertStringToDateFormat(stringDate);
+
+    assertThat(convertedDate).isEqualTo(LocalDate.parse("01-01-2025", outputFormat));
+    assertThat(convertedDate.format(outputFormat)).isEqualTo("01-01-2025");
+  }
+
+  @Test
+  @DisplayName("convertDateFromDatabaseToRightFormat - Should successfully convert a date coming from database into the right format")
+  void convertDateFromDatabaseToRightFormatSuccess() {
+    DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    LocalDate inputDate = LocalDate.parse("2025-01-01", inputFormat);
+
+    String formattedDate = ConvertDateFormat.convertDateFromDatabaseToRightFormat(inputDate);
+
+    assertThat(formattedDate).isEqualTo("01-01-2025");
+  }
+}
