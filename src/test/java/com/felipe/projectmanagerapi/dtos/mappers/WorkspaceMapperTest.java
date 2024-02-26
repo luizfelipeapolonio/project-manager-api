61292f5b80de1601feff7c2c1cd20db1aaca9828
Luiz Felipe <luizfelipe0527@gmail.com>
2024-02-26 16:58:12 -0300

    test: add convert Workspace entity to WorkspaceFullResponseDTO test case

--- src/test/java/com/felipe/projectmanagerapi/dtos/mappers/WorkspaceMapperTest.java
+++ src/test/java/com/felipe/projectmanagerapi/dtos/mappers/WorkspaceMapperTest.java
@@ -1,6 +1,11 @@
 package com.felipe.projectmanagerapi.dtos.mappers;
 
+import com.felipe.projectmanagerapi.dtos.ProjectResponseDTO;
+import com.felipe.projectmanagerapi.dtos.UserResponseDTO;
+import com.felipe.projectmanagerapi.dtos.WorkspaceFullResponseDTO;
 import com.felipe.projectmanagerapi.dtos.WorkspaceResponseDTO;
+import com.felipe.projectmanagerapi.models.Project;
+import com.felipe.projectmanagerapi.models.User;
 import com.felipe.projectmanagerapi.models.Workspace;
 import com.felipe.projectmanagerapi.utils.GenerateMocks;
 import org.junit.jupiter.api.AfterEach;
@@ -11,6 +16,7 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -52,5 +58,27 @@ public class WorkspaceMapperTest {
     assertThat(convertedWorkspaceDTO.ownerId()).isEqualTo(workspaceResponseDTO.ownerId());
     assertThat(convertedWorkspaceDTO.createdAt()).isEqualTo(workspaceResponseDTO.createdAt());
     assertThat(convertedWorkspaceDTO.updatedAt()).isEqualTo(workspaceResponseDTO.updatedAt());
+  }
+
+  @Test
+  @DisplayName("toWorkspaceFullResponseDTO - Should successfully convert a workspace entity into a WorkspaceFullResponseDTO")
+  void convertEntityToWorkspaceFullResponseDTOSuccess() {
+    Project projectMock = this.dataMock.getProjects().get(1);
+    Workspace workspace = this.dataMock.getWorkspaces().get(0);
+    workspace.setProjects(List.of(projectMock));
+
+    WorkspaceFullResponseDTO convertedWorkspace = this.workspaceMapper.toWorkspaceFullResponseDTO(workspace);
+
+    assertThat(convertedWorkspace.workspace().id()).isEqualTo(workspace.getId());
+    assertThat(convertedWorkspace.workspace().name()).isEqualTo(workspace.getName());
+    assertThat(convertedWorkspace.workspace().ownerId()).isEqualTo(workspace.getOwner().getId());
+    assertThat(convertedWorkspace.workspace().createdAt()).isEqualTo(workspace.getCreatedAt());
+    assertThat(convertedWorkspace.workspace().updatedAt()).isEqualTo(workspace.getUpdatedAt());
+    assertThat(convertedWorkspace.members().stream().map(UserResponseDTO::id).toList())
+      .containsExactlyInAnyOrderElementsOf(workspace.getMembers().stream().map(User::getId).toList());
+    assertThat(convertedWorkspace.members().size()).isEqualTo(workspace.getMembers().size());
+    assertThat(convertedWorkspace.projects().stream().map(ProjectResponseDTO::id).toList())
+      .containsExactlyInAnyOrderElementsOf(workspace.getProjects().stream().map(Project::getId).toList());
+    assertThat(convertedWorkspace.projects().size()).isEqualTo(workspace.getProjects().size());
   }
 }
