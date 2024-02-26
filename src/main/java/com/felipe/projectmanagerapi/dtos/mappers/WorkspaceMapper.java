e9d1088020359f0e15c3882efce8eb8ffe2632cb
Luiz Felipe <luizfelipe0527@gmail.com>
2024-02-26 16:57:20 -0300

    feat: add convert Workspace entity to WorkspaceFullResponseDTO function

--- src/main/java/com/felipe/projectmanagerapi/dtos/mappers/WorkspaceMapper.java
+++ src/main/java/com/felipe/projectmanagerapi/dtos/mappers/WorkspaceMapper.java
@@ -1,8 +1,14 @@
 package com.felipe.projectmanagerapi.dtos.mappers;
 
+import com.felipe.projectmanagerapi.dtos.ProjectResponseDTO;
+import com.felipe.projectmanagerapi.dtos.UserResponseDTO;
+import com.felipe.projectmanagerapi.dtos.WorkspaceFullResponseDTO;
 import com.felipe.projectmanagerapi.dtos.WorkspaceResponseDTO;
 import com.felipe.projectmanagerapi.models.Workspace;
+import com.felipe.projectmanagerapi.utils.ConvertDateFormat;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class WorkspaceMapper {
@@ -15,5 +21,38 @@ public class WorkspaceMapper {
       workspace.getCreatedAt(),
       workspace.getUpdatedAt()
     );
+  }
+
+  public WorkspaceFullResponseDTO toWorkspaceFullResponseDTO(Workspace workspace) {
+    if(workspace == null) return null;
+    WorkspaceResponseDTO workspaceDTO = this.toDTO(workspace);
+    List<ProjectResponseDTO> projects = workspace.getProjects()
+      .stream()
+      .map(project -> new ProjectResponseDTO(
+        project.getId(),
+        project.getName(),
+        project.getPriority().getValue(),
+        project.getCategory(),
+        project.getDescription(),
+        project.getBudget().toString(),
+        ConvertDateFormat.convertDateToFormattedString(project.getDeadline()),
+        project.getCreatedAt(),
+        project.getUpdatedAt(),
+        project.getOwner().getId(),
+        project.getWorkspace().getId()
+      ))
+      .toList();
+    List<UserResponseDTO> members = workspace.getMembers()
+      .stream()
+      .map(member -> new UserResponseDTO(
+        member.getId(),
+        member.getName(),
+        member.getEmail(),
+        member.getRole().getName(),
+        member.getCreatedAt(),
+        member.getUpdatedAt()
+      ))
+      .toList();
+    return new WorkspaceFullResponseDTO(workspaceDTO, projects, members);
   }
 }
