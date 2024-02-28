241893262bf559e0a72326eac92d4d9602cd8471
Luiz Felipe <luizfelipe0527@gmail.com>
2024-02-27 23:17:45 -0300

    feat: implement project update function

--- src/main/java/com/felipe/projectmanagerapi/services/ProjectService.java
+++ src/main/java/com/felipe/projectmanagerapi/services/ProjectService.java
@@ -1,8 +1,10 @@
 package com.felipe.projectmanagerapi.services;
 
 import com.felipe.projectmanagerapi.dtos.ProjectCreateDTO;
+import com.felipe.projectmanagerapi.dtos.ProjectUpdateDTO;
 import com.felipe.projectmanagerapi.dtos.mappers.ProjectMapper;
 import com.felipe.projectmanagerapi.exceptions.InvalidDateException;
+import com.felipe.projectmanagerapi.exceptions.RecordNotFoundException;
 import com.felipe.projectmanagerapi.infra.security.AuthorizationService;
 import com.felipe.projectmanagerapi.infra.security.UserPrincipal;
 import com.felipe.projectmanagerapi.models.Project;
@@ -11,6 +13,7 @@ import com.felipe.projectmanagerapi.repositories.ProjectRepository;
 import com.felipe.projectmanagerapi.utils.ConvertDateFormat;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
@@ -64,5 +67,47 @@ public class ProjectService {
     newProject.setWorkspace(currentWorkspace);
 
     return this.projectRepository.save(newProject);
+  }
+
+  public Project update(@NotNull String projectId, @NotNull @Valid ProjectUpdateDTO projectUpdate) {
+    Authentication authentication = this.authorizationService.getAuthentication();
+    UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+    LocalDate today = LocalDate.now();
+
+    return this.projectRepository.findById(projectId)
+      .map(project -> {
+        if(!project.getOwner().getId().equals(userPrincipal.getUser().getId())) {
+          throw new AccessDeniedException("Acesso negado: Você não tem permissão para alterar este recurso");
+        }
+
+        if(projectUpdate.name() != null) {
+          project.setName(projectUpdate.name());
+        }
+        if(projectUpdate.category() != null) {
+          project.setCategory(projectUpdate.category());
+        }
+        if(projectUpdate.description() != null) {
+          project.setDescription(projectUpdate.description());
+        }
+        if(projectUpdate.budget() != null) {
+          project.setBudget(projectUpdate.budget());
+        }
+        if(projectUpdate.priority() != null) {
+          project.setPriority(this.projectMapper.convertValueToPriorityLevel(projectUpdate.priority()));
+        }
+        if(projectUpdate.deadline() != null) {
+          LocalDate deadlineToUpdateDate = ConvertDateFormat.convertFormattedStringToDate(projectUpdate.deadline());
+          if(deadlineToUpdateDate.isBefore(today)) {
+            throw new InvalidDateException(
+              "Data inválida. O prazo de entrega do projeto não deve ser antes da data atual" +
+              "\nData atual: " + today.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) +
+              "\nPrazo do projeto: " + projectUpdate.deadline()
+            );
+          }
+          project.setDeadline(ConvertDateFormat.convertFormattedStringToDate(projectUpdate.deadline()));
+        }
+        return this.projectRepository.save(project);
+      })
+      .orElseThrow(() -> new RecordNotFoundException("Projeto de ID: '" + projectId + "' não encontrado"));
   }
 }
