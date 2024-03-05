3b3847d40e00c729aa25bdf0de86a79c5fc0c69b
Luiz Felipe <luizfelipe0527@gmail.com>
2024-03-04 21:45:37 -0300

    feat: implement get all projects by workspace id and owner id function

--- src/main/java/com/felipe/projectmanagerapi/repositories/ProjectRepository.java
+++ src/main/java/com/felipe/projectmanagerapi/repositories/ProjectRepository.java
@@ -2,6 +2,13 @@ package com.felipe.projectmanagerapi.repositories;
 
 import com.felipe.projectmanagerapi.models.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, String> {
+
+  @Query("SELECT p FROM Project p WHERE p.workspace.id=:workspaceId AND p.owner.id=:ownerId")
+  List<Project> findAllByWorkspaceIdAndOwnerId(@Param("workspaceId") String workspaceId, @Param("ownerId") String ownerId);
 }
