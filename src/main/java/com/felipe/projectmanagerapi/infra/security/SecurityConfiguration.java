f5ff357502d1f50785fe79ae90156d48e7fc91ad
Luiz Felipe <luizfelipe0527@gmail.com>
2024-03-06 22:04:29 -0300

    feat: add get project by id authorization rule

--- src/main/java/com/felipe/projectmanagerapi/infra/security/SecurityConfiguration.java
+++ src/main/java/com/felipe/projectmanagerapi/infra/security/SecurityConfiguration.java
@@ -59,6 +59,7 @@ public class SecurityConfiguration {
         .requestMatchers(HttpMethod.POST, projectBaseUrl).hasAnyRole("ADMIN", "WRITE_READ")
         .requestMatchers(HttpMethod.PATCH, projectBaseUrl + "/{projectId}").hasAnyRole("ADMIN", "WRITE_READ")
         .requestMatchers(projectBaseUrl + "/workspace/**").hasRole("ADMIN")
+        .requestMatchers(HttpMethod.GET, projectBaseUrl + "/{projectId}/workspace/**").hasAnyRole("ADMIN", "WRITE_READ", "READ_ONLY")
         .requestMatchers(HttpMethod.POST, "/api/projects/test").permitAll()
         .anyRequest().authenticated())
       .addFilterBefore(this.securityFilter, UsernamePasswordAuthenticationFilter.class)
