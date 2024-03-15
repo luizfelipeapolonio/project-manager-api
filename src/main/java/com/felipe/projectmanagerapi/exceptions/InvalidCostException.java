4f7226efa5acf0edd5bd0c40c9b5a2db0e3e4c86
Luiz Felipe <luizfelipe0527@gmail.com>
2024-03-15 17:58:33 -0300

    feat: add InvalidCostException

---
+++ src/main/java/com/felipe/projectmanagerapi/exceptions/InvalidCostException.java
@@ -0,0 +1,7 @@
+package com.felipe.projectmanagerapi.exceptions;
+
+public class InvalidCostException extends RuntimeException {
+  public InvalidCostException(String message) {
+    super(message);
+  }
+}
