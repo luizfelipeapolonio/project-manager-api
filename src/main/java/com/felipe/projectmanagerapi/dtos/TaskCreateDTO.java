3e2bb646fc1eb44d794ecf8dd99d5fc078af2d92
Luiz Felipe <luizfelipe0527@gmail.com>
2024-03-15 22:17:58 -0300

    improvement: add field validation

--- src/main/java/com/felipe/projectmanagerapi/dtos/TaskCreateDTO.java
+++ src/main/java/com/felipe/projectmanagerapi/dtos/TaskCreateDTO.java
@@ -1,4 +1,26 @@
 package com.felipe.projectmanagerapi.dtos;
 
-public record TaskCreateDTO(String name, String description, String cost, String projectId) {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
+
+public record TaskCreateDTO(
+  @NotNull(message = "O nome não deve ser nulo")
+  @NotBlank(message = "O nome não deve estar em branco")
+  @Length(min = 1, max = 70, message = "O nome deve ter entre 1 e 70 caracteres")
+  String name,
+
+  @NotNull(message = "A descrição não deve ser nula")
+  @NotBlank(message = "A descrição não deve estar em branco")
+  String description,
+
+  @NotNull(message = "O custo não deve ser nulo")
+  @NotBlank(message = "O custo não deve estar em branco")
+  @Pattern(regexp = "^\\d+\\.\\d{2}$", message = "Custo inválido! Digite no formato válido. Ex: 1200.00")
+  String cost,
+
+  @NotNull(message = "O ID do projeto não deve ser nulo")
+  @NotBlank(message = "O ID do projeto não deve estar em branco")
+  String projectId
+) {}
