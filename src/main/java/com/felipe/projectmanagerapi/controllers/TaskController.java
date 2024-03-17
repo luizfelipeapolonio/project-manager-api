9c78a4da8665272da9ca32c5204b7af160e9c34a
Luiz Felipe <luizfelipe0527@gmail.com>
2024-03-16 21:56:00 -0300

    feat: implement task create controller

--- src/main/java/com/felipe/projectmanagerapi/controllers/TaskController.java
+++ src/main/java/com/felipe/projectmanagerapi/controllers/TaskController.java
@@ -1,29 +1,45 @@
 package com.felipe.projectmanagerapi.controllers;
 
 import com.felipe.projectmanagerapi.dtos.TaskCreateDTO;
+import com.felipe.projectmanagerapi.dtos.TaskResponseDTO;
+import com.felipe.projectmanagerapi.dtos.mappers.TaskMapper;
+import com.felipe.projectmanagerapi.enums.ResponseConditionStatus;
 import com.felipe.projectmanagerapi.models.Task;
 import com.felipe.projectmanagerapi.services.TaskService;
+import com.felipe.projectmanagerapi.utils.CustomResponseBody;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
 
   private final TaskService taskService;
+  private final TaskMapper taskMapper;
 
-  public TaskController(TaskService taskService) {
+  public TaskController(TaskService taskService, TaskMapper taskMapper) {
     this.taskService = taskService;
+    this.taskMapper = taskMapper;
   }
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  public String create(@RequestBody TaskCreateDTO task) {
+  public CustomResponseBody<TaskResponseDTO> create(@RequestBody @Valid TaskCreateDTO task) {
     Task createdTask = this.taskService.create(task);
-    return "New Budget: %s" + createdTask.getProject().getBudget();
+    TaskResponseDTO taskResponseDTO = this.taskMapper.toDTO(createdTask);
+
+    CustomResponseBody<TaskResponseDTO> response = new CustomResponseBody<>();
+    response.setStatus(ResponseConditionStatus.SUCCESS);
+    response.setCode(HttpStatus.CREATED);
+    response.setMessage("Task criada com sucesso");
+    response.setData(taskResponseDTO);
+    return response;
   }
 }
