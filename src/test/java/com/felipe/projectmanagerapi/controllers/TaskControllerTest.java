3513d75dc7582051e108d2d7362d72ffe10ee16e
Luiz Felipe <luizfelipe0527@gmail.com>
2024-03-21 20:46:48 -0300

    test: add get task by id test cases

--- src/test/java/com/felipe/projectmanagerapi/controllers/TaskControllerTest.java
+++ src/test/java/com/felipe/projectmanagerapi/controllers/TaskControllerTest.java
@@ -5,6 +5,7 @@ import com.felipe.projectmanagerapi.dtos.TaskCreateDTO;
 import com.felipe.projectmanagerapi.dtos.TaskResponseDTO;
 import com.felipe.projectmanagerapi.dtos.mappers.TaskMapper;
 import com.felipe.projectmanagerapi.enums.ResponseConditionStatus;
+import com.felipe.projectmanagerapi.exceptions.RecordNotFoundException;
 import com.felipe.projectmanagerapi.models.Task;
 import com.felipe.projectmanagerapi.services.TaskService;
 import com.felipe.projectmanagerapi.utils.GenerateMocks;
@@ -20,16 +21,19 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.any;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
@@ -106,5 +110,78 @@ public class TaskControllerTest {
 
     verify(this.taskService, times(1)).create(taskCreateDTO);
     verify(this.taskMapper, times(1)).toDTO(task);
+  }
+
+  @Test
+  @DisplayName("getById - Should return a success response with OK status code and the task")
+  void getByIdSuccess() throws Exception {
+    Task task = this.dataMock.getTasks().get(0);
+    TaskResponseDTO taskResponseDTO = new TaskResponseDTO(
+      task.getId(),
+      task.getName(),
+      task.getDescription(),
+      task.getCost().toString(),
+      task.getCreatedAt(),
+      task.getUpdatedAt(),
+      task.getProject().getId(),
+      task.getOwner().getId()
+    );
+
+    when(this.taskService.getById("01")).thenReturn(task);
+    when(this.taskMapper.toDTO(task)).thenReturn(taskResponseDTO);
+
+    this.mockMvc.perform(get(BASE_URL + "/01")
+      .accept(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.status").value(ResponseConditionStatus.SUCCESS.getValue()))
+      .andExpect(jsonPath("$.code").value(HttpStatus.OK.value()))
+      .andExpect(jsonPath("$.message").value("Task encontrada"))
+      .andExpect(jsonPath("$.data.id").value(taskResponseDTO.id()))
+      .andExpect(jsonPath("$.data.name").value(taskResponseDTO.name()))
+      .andExpect(jsonPath("$.data.description").value(taskResponseDTO.description()))
+      .andExpect(jsonPath("$.data.cost").value(taskResponseDTO.cost()))
+      .andExpect(jsonPath("$.data.createdAt").value(taskResponseDTO.createdAt().toString()))
+      .andExpect(jsonPath("$.data.updatedAt").value(taskResponseDTO.updatedAt().toString()))
+      .andExpect(jsonPath("$.data.projectId").value(taskResponseDTO.projectId()))
+      .andExpect(jsonPath("$.data.ownerId").value(taskResponseDTO.ownerId()));
+
+    verify(this.taskService, times(1)).getById("01");
+    verify(this.taskMapper, times(1)).toDTO(task);
+  }
+
+  @Test
+  @DisplayName("getById - Should return an error response with not found status code")
+  void getByIdFailsByTaskNotFound() throws Exception {
+    when(this.taskService.getById("01"))
+      .thenThrow(new RecordNotFoundException("Task de ID: '01' não encontrada"));
+
+    this.mockMvc.perform(get(BASE_URL + "/01")
+      .accept(MediaType.APPLICATION_JSON))
+      .andExpect(status().isNotFound())
+      .andExpect(jsonPath("$.status").value(ResponseConditionStatus.ERROR.getValue()))
+      .andExpect(jsonPath("$.code").value(HttpStatus.NOT_FOUND.value()))
+      .andExpect(jsonPath("$.message").value("Task de ID: '01' não encontrada"))
+      .andExpect(jsonPath("$.data").doesNotExist());
+
+    verify(this.taskService, times(1)).getById("01");
+    verify(this.taskMapper, never()).toDTO(any(Task.class));
+  }
+
+  @Test
+  @DisplayName("getById - Should return an error response with forbidden status code")
+  void getByIdFailsByNotBeingWorkspaceOwnerOrMember() throws Exception {
+    when(this.taskService.getById("01"))
+      .thenThrow(new AccessDeniedException("Acesso negado: Você não tem permissão para acessar este recurso"));
+
+    this.mockMvc.perform(get(BASE_URL + "/01")
+      .accept(MediaType.APPLICATION_JSON))
+      .andExpect(status().isForbidden())
+      .andExpect(jsonPath("$.status").value(ResponseConditionStatus.ERROR.getValue()))
+      .andExpect(jsonPath("$.code").value(HttpStatus.FORBIDDEN.value()))
+      .andExpect(jsonPath("$.message").value("Acesso negado: Você não tem permissão para acessar este recurso"))
+      .andExpect(jsonPath("$.data").doesNotExist());
+
+    verify(this.taskService, times(1)).getById("01");
+    verify(this.taskMapper, never()).toDTO(any(Task.class));
   }
 }
--- src/test/java/com/felipe/projectmanagerapi/services/TaskServiceTest.java
+++ src/test/java/com/felipe/projectmanagerapi/services/TaskServiceTest.java
@@ -1,10 +1,12 @@
 package com.felipe.projectmanagerapi.services;
 
 import com.felipe.projectmanagerapi.dtos.TaskCreateDTO;
+import com.felipe.projectmanagerapi.exceptions.RecordNotFoundException;
 import com.felipe.projectmanagerapi.infra.security.AuthorizationService;
 import com.felipe.projectmanagerapi.infra.security.UserPrincipal;
 import com.felipe.projectmanagerapi.models.Project;
 import com.felipe.projectmanagerapi.models.Task;
+import com.felipe.projectmanagerapi.models.Workspace;
 import com.felipe.projectmanagerapi.repositories.TaskRepository;
 import com.felipe.projectmanagerapi.utils.GenerateMocks;
 import org.junit.jupiter.api.AfterEach;
@@ -15,10 +17,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
@@ -26,6 +31,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doNothing;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchException;
 
 public class TaskServiceTest {
 
@@ -97,5 +103,75 @@ public class TaskServiceTest {
     verify(this.projectService, times(1)).getById("02");
     verify(this.projectService, times(1)).addCost(project, cost);
     verify(this.taskRepository, times(1)).save(any(Task.class));
+  }
+
+  @Test
+  @DisplayName("getById - Should successfully get a task of a project")
+  void getByIdSuccess() {
+    UserPrincipal userPrincipal = new UserPrincipal(this.dataMock.getUsers().get(2));
+    Task task = this.dataMock.getTasks().get(0);
+
+    when(this.authorizationService.getAuthentication()).thenReturn(this.authentication);
+    when(this.authentication.getPrincipal()).thenReturn(userPrincipal);
+    when(this.taskRepository.findById("01")).thenReturn(Optional.of(task));
+
+    Task foundTask = this.taskService.getById("01");
+
+    assertThat(foundTask.getId()).isEqualTo(task.getId());
+    assertThat(foundTask.getName()).isEqualTo(task.getName());
+    assertThat(foundTask.getDescription()).isEqualTo(task.getDescription());
+    assertThat(foundTask.getCost()).isEqualTo(task.getCost());
+    assertThat(foundTask.getCreatedAt()).isEqualTo(task.getCreatedAt());
+    assertThat(foundTask.getUpdatedAt()).isEqualTo(task.getUpdatedAt());
+    assertThat(foundTask.getProject().getId()).isEqualTo(task.getProject().getId());
+    assertThat(foundTask.getOwner().getId()).isEqualTo(task.getOwner().getId());
+
+    verify(this.authorizationService, times(1)).getAuthentication();
+    verify(this.authentication, times(1)).getPrincipal();
+    verify(this.taskRepository, times(1)).findById("01");
+  }
+
+  @Test
+  @DisplayName("getById - Should throw a RecordNotFoundException if the task is not found")
+  void getByIdFailsByTaskNotFound() {
+    UserPrincipal userPrincipal = new UserPrincipal(this.dataMock.getUsers().get(2));
+
+    when(this.authorizationService.getAuthentication()).thenReturn(this.authentication);
+    when(this.authentication.getPrincipal()).thenReturn(userPrincipal);
+    when(this.taskRepository.findById("01")).thenReturn(Optional.empty());
+
+    Exception thrown = catchException(() -> this.taskService.getById("01"));
+
+    assertThat(thrown)
+      .isExactlyInstanceOf(RecordNotFoundException.class)
+      .hasMessage("Task de ID: '01' não encontrada");
+
+    verify(this.authorizationService, times(1)).getAuthentication();
+    verify(this.authentication, times(1)).getPrincipal();
+    verify(this.taskRepository, times(1)).findById("01");
+  }
+
+  @Test
+  @DisplayName("getById - Should throw an AccessDeniedException if the authenticated user is not the workspace owner or member")
+  void getByIdFailsByNotBeingWorkspaceOwnerOrMember() {
+    UserPrincipal userPrincipal = new UserPrincipal(this.dataMock.getUsers().get(2));
+    Task task = this.dataMock.getTasks().get(0);
+
+    Workspace workspace = this.dataMock.getWorkspaces().get(0);
+    workspace.setMembers(List.of(this.dataMock.getUsers().get(1)));
+
+    when(this.authorizationService.getAuthentication()).thenReturn(this.authentication);
+    when(this.authentication.getPrincipal()).thenReturn(userPrincipal);
+    when(this.taskRepository.findById("01")).thenReturn(Optional.of(task));
+
+    Exception thrown = catchException(() -> this.taskService.getById("01"));
+
+    assertThat(thrown)
+      .isExactlyInstanceOf(AccessDeniedException.class)
+      .hasMessage("Acesso negado: Você não tem permissão para acessar este recurso");
+
+    verify(this.authorizationService, times(1)).getAuthentication();
+    verify(this.authentication, times(1)).getPrincipal();
+    verify(this.taskRepository, times(1)).findById("01");
   }
 }
