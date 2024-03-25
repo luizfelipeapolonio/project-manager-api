a807f3668a8d43261578bedfb2b3f96db851e0e4
Luiz Felipe <luizfelipe0527@gmail.com>
2024-03-25 15:28:46 -0300

    test(improvement): adjust imports

--- src/test/java/com/felipe/projectmanagerapi/repositories/ProjectRepositoryTest.java
+++ src/test/java/com/felipe/projectmanagerapi/repositories/ProjectRepositoryTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
