45ccdcfce301e926ee420d5b38d66429f438ba0c
Luiz Felipe <luizfelipe0527@gmail.com>
2024-03-25 15:29:54 -0300

    feat: add findAllByProjectId method

--- src/main/java/com/felipe/projectmanagerapi/repositories/TaskRepository.java
+++ src/main/java/com/felipe/projectmanagerapi/repositories/TaskRepository.java
@@ -2,6 +2,13 @@ package com.felipe.projectmanagerapi.repositories;
 
 import com.felipe.projectmanagerapi.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, String> {
+
+  @Query("SELECT t FROM Task t WHERE t.project.id=:projectId")
+  List<Task> findAllByProjectId(@Param("projectId") String projectId);
 }
